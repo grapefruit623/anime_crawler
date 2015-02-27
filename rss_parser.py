450d551467d966b7b3e6129f22816a473ef26ac2
grapefruit623 <grapefruit623@gmail.com>
2015-02-27 22:07:55 +0800

    Using os.fork() to create a son process launches transmission-gtk, then the parent process will add_torent
    Issue:
        I use time.sleep() function to ensure transmission-gtk have been launched.
        Does any way to check whether transmission-gtk is running?

--- rss_parser.py
+++ rss_parser.py
@@ -1,25 +1,29 @@
 #! -*-coding: utf-8 -*-
-import feedparser
-import urllib
-import urllib2
+import os
 import re
+import urllib
 
+import feedparser
 import transmissionrpc
+import time
+
 from BeautifulSoup import BeautifulSoup
 
 userName = 'grf623BT'
 passwd = '1234567'
-tc = transmissionrpc.Client('localhost', port=9091, user=userName, password=passwd)
+
 
 class rss_parser():
     def __init__(self):
         self.parser = feedparser.parse('http://share.popgo.org/rss/rss.xml')
-    
-    def print_all(self):
-        print self.parser
+        if self.open_transmissionClient(): # Father will add torent
+            time.sleep(3)
+            self.tc = transmissionrpc.Client('localhost', port=9091,
+                                             user=userName, password=passwd)
+            self.parse_entries()
 
-    def print_feed(self):
-        print self.parser.feed
+    def __del__(self):
+        print '__del__'
 
     def targetList(self):
         f = open('targetList.txt', 'r')
@@ -32,29 +36,39 @@ class rss_parser():
         return target
     '''
         Can get magnet href
+        We must be check whether transmission is launched?
     '''
     def parse_entries(self):
         entries = self.parser.entries
         '''
-        htmlRes = urllib.urlopen(entries[0].link).read()
-        magnet = BeautifulSoup(htmlRes).find( name='a', attrs={'href': re.compile('magnet')} )
-        print magnet['href']
-
-        tc.add_torrent( magnet['href'] )
-        '''
-
-        '''
             Can use title to filter animation and attr i.e. BIG5, MP4
         '''
-        #keywordList = self.targetList()
-        #print keywordList
+        # keywordList = self.targetList()
+        # print keywordList
         for item in entries:
             # u'' is meaning?
-            if None != re.search( re.compile(u'絕對|繁體'), item.title ):
-                print item.title 
+            if None != re.search(re.compile(u'絕對|繁體'), item.title):
+                htmlRes = urllib.urlopen(item.link).read()
+                magnet = BeautifulSoup(htmlRes).find(name='a',
+                                                     attrs={'href': re.compile('magnet')})
+                self.tc.add_torrent(magnet['href'])
+
+    '''
+        must be mult thresad
+    '''
+    def open_transmissionClient(self):
+        pid = os.fork()
+        if 0 == pid:
+            print 'child launch transmission-gtk'
+            os.system('transmission-gtk')
+            return 0
+        else:
+            print 'father pid: ', pid
+            return 1
 
 if __name__ == '__main__':
     rssParser = rss_parser()
-    #rss.print_all()
-    rssParser.parse_entries()
-    pass
+    # rss.print_all()
+    # rssParser.open_transmissionClient()
+    # rssParser.parse_entries()
+    print '__main__'
